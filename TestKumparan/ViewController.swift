3d7b43575ff17baaa3cc45e9ccbf94ba37ba4b7c
Linando Saputra <linandosaputra@yahoo.co.id>
2021-09-04 21:45:58 +0700

    Initial Commit

---
+++ TestKumparan/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestKumparan
+//
+//  Created by Linando Saputra on 04/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
